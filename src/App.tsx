2bf5b35e168902635217f8412cb9d554fd5eed60
metathink <jmpmetaimpthink@gmail.com>
2024-09-17 15:10:05 +0900

    antd add

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,56 @@
-import { Layout, Col, Row } from 'antd';
-import AppBar from "./components/AppBar";
-import PostContainer from "./components/PostContainer/PostContainer";
-import FooterContainer from "./components/FooterContainer";
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme, Input } from 'antd';
+
+const { Search } = Input;
 
 const { Header, Content, Footer } = Layout;
 
-function App() {
+const items = [
+  { key: '1', label: 'Title' },
+  { key: '2', label: 'Tags' },
+  { key: '3', label: 'Item' },
+];
+
+const onSearch = () => {
+  
+}
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Header>
-        <AppBar />
+    <Layout>
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="demo-logo" />
+        <Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          style={{ flex: 1, minWidth: 0 }}
+        />
       </Header>
-      <Layout>
-        <Content style={{ padding: '0 24px', minHeight: 280 }}>
-          <Row justify="center">
-            <Col xs={24} sm={22} md={18} lg={16} xl={14}>
-              <PostContainer />
-            </Col>
-          </Row>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          <FooterContainer />
-        </Footer>
-      </Layout>
+      <Content style={{ padding: '0 48px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+        </Breadcrumb>
+        <div
+          style={{
+            background: colorBgContainer,
+            minHeight: 280,
+            padding: 24,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+      </Footer>
     </Layout>
   );
-}
+};
 
 export default App;
