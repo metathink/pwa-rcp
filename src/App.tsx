d04432a696cb08bb665272a396b6625db0a12248
metathink <jmpmetaimpthink@gmail.com>
2024-09-17 14:52:21 +0900

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import AppBar from "./components/AppBar"
-
 import styled from "styled-components";
+import AppBar from "./components/AppBar";
 import PostContainer from "./components/PostContainer/PostContainer";
 import FooterContainer from "./components/FooterContainer";
 
 export const AppContainer = styled.div`
     height: 100vh;
-    width: 100%;
-    margin: 5px;
+    width: 1200px; /* Fixed width */
+    margin: 5px auto; /* Center the container */
     padding: 5px;
 
+    /* Optional background styles */
     /* background-color: beige; 
     background-image: 
         linear-gradient(45deg, rgba(255, 255, 255, 0.4) 25%, transparent 25%, transparent 75%, rgba(255, 255, 255, 0.4) 75%),
         linear-gradient(45deg, rgba(255, 255, 255, 0.4) 25%, transparent 25%, transparent 75%, rgba(255, 255, 255, 0.4) 75%);
     background-size: 40px 40px; 
     background-position: 0 0, 20px 20px;  */
-
 `;
 
 function App() {
-
   return (
     <>
       <AppContainer>
